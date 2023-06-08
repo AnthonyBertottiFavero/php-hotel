973483e2206c4ea9484df3dcc6ddf4f5f425831f
AnthonyBertottiFavero <bertottianthony1@gmail.com>
2023-06-08 16:03:48 +0200

    add commit

---
+++ index.php
@@ -0,0 +1,117 @@
+<?php
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+]; 
+$park = isset($_GET['park']) ? $_GET['park'] : '';
+$vote = isset($_GET['vote']) ? $_GET['vote'] : '';
+
+// LOGICA PER FILTRO HOTEL
+
+if ($park === 'true') {
+    $filteredHotels = array_filter($hotels, function ($hotel) use ($vote) {
+        return $hotel['parking'] === true && $hotel['vote'] >= $vote;
+    });
+} elseif ($park === 'false') {
+    $filteredHotels = array_filter($hotels, function ($hotel) use ($vote) {
+        return $hotel['parking'] === false && $hotel['vote'] >= $vote;
+    });
+} else {
+    $filteredHotels = array_filter($hotels, function ($hotel) use ($vote) {
+        return $hotel['vote'] >= $vote;
+    });
+}
+?>
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotels</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
+</head>
+
+<body>
+    <form action="" class="d-flex p-2" style="gap:1.5rem" method="get">
+        <label for="select">Parcheggio</label>
+        <select class="form-select w-25 align-items-center" aria-label="Default select example" id="select" name="park">
+            <option selected>Scegli</option>
+            <option value="true">Si</option>
+            <option value="false">No</option>
+        </select>
+
+        <label for="vote">Voto</label>
+        <input type="number" name="vote" id="vote">
+
+        <button type="submit" class="btn btn-primary">Cerca</button>
+
+        <button type="button" class="btn btn-secondary">Annulla</button>
+    </form>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Stelle</th>
+                <th scope="col">Distanza dal centro (km)</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($filteredHotels as $key => $hotel) {?>
+            <tr>
+                <th scope="row"><?= $hotel["name"] ?></th>
+                <td><?= $hotel['description'] ?></td>
+                <td><?php 
+                    if ($hotels[$key]['parking'] == true){ ?>
+                        SI <?php
+                    } else { ?>
+                        NO <?php
+                    };
+                ?></td>
+                <td><?= $hotel['vote'] ?></td>
+                <td><?= $hotel['distance_to_center'] ?></td>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</body>
+
+</html>
